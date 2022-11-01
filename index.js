3066020ec03583823ebeff978a54569054af937b
ujvarivera <ujvariveronika0320@gmail.com>
2022-11-01 21:14:48 +0100

    minimal books api created

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const express = require('express')
+import { books } from './books.js'
+import { books_with_images } from './books_with_images.js'
+import express from 'express'
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+
+/* returns the books without the images  */
+app.get('/', async(req, res) => {
+    res.json(books.books)
 })
+
+/* returns the books with images  */
+app.get('/books', async(req, res) => {
+    res.json(books_with_images.results)
+})
+
+/* returns a random book */
+app.get('/random', async(req, res) => {
+    res.json(books_with_images.results[Math.floor(Math.random()*books_with_images.results.length)]);
+})
+
 app.listen(process.env.PORT || 3000)
