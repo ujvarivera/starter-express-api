f7837636a5e91649c7d8f8decc1d1d57730743c0
ujvarivera <ujvariveronika0320@gmail.com>
2022-11-19 11:56:52 +0100

    another books' adatset is used, index file updated

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { books } from './books.js'
-import { books_with_images } from './books_with_images.js'
+import { books } from './bestBooks.js'
 import express from 'express'
 
 const app = express()
@@ -8,41 +7,224 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: err })
 })
 
-/* returns all the books without the images  */
-/*
-app.get('/', (req, res) => {
-    res.json(books.books)
-})
-*/
-
-/* returns a random book without the images  */
-app.get('/randombook', (req, res) => {
-    res.json(books.books[Math.floor(Math.random()*books.books.length)])
-})
-
 /* returns a random book */
 app.get('/random', (req, res) => {
-    res.json(books_with_images.results[Math.floor(Math.random()*books_with_images.results.length)]);
-})
-
-/* returns the first X books without the images  */
-app.get('/:num', (req, res) => {
-    const num = req.params.num;
-    res.json(books.books.slice(0, num))
+    res.json(books.results[Math.floor(Math.random()*books.results.length)])
 })
 
 /* returns random books of given number */
 app.get('/random/:num', (req, res) => {
     const num = req.params.num;
-    const shuffled = books_with_images.results.sort(() => 0.5 - Math.random());
+    const shuffled = books.results.sort(() => 0.5 - Math.random());
     res.json(shuffled.slice(0, num));
-    console.log(res.status);
+})
+
+/* returns information about books by a given title (should not be exact match)*/
+app.get('/bookinfo/:title', (req, res) => {
+    let title = req.params.title;
+    let booksOfGivenTitle = [];
+
+    for (const book of books.results) {
+        if (book.title.toLowerCase().includes(title.toLowerCase())) { // eval: "['Horror']" => ['Horror']
+            booksOfGivenTitle.push(book);
+        }
+    }
+
+    res.json(booksOfGivenTitle); // returns a list with more or one object(s)
+})
+
+/* returns information about A BOOK by a given title (should be EXACT match, although case-insensitive)*/
+app.get('/bookinfo/exact/:title', (req, res) => {
+    let title = req.params.title;
+
+    for (const book of books.results) {
+        if (book.title.toLowerCase() === title.toLowerCase()) { // eval: "['Horror']" => ['Horror']
+            return res.json(book); // returns only an object
+        }
+    }
+
+    res.json({});
+})
+
+/* returns books by the ratings in desc order */
+app.get('/bestbooks', (req, res) => {
+
+    books.results = books.results.sort((a, b) => (a.rating > b.rating) ? -1 : 1);
+
+    res.json(books.results);
+})
+
+/* returns the top X best books */
+app.get('/bestbooks/:num', (req, res) => {
+    let num = req.params.num;
+    books.results = books.results.sort((a, b) => (a.rating > b.rating) ? -1 : 1);
+
+    res.json(books.results.slice(0, num)); // returns a list of objects
 })
 
 /* returns the first X books */
-app.get('/books/:num', (req, res) => {
+app.get('/:num', (req, res) => {
     const num = req.params.num;
-    res.json(books_with_images.results.slice(0,num));
+    res.json(books.results.slice(0, num)) // returns a list of objects
+})
+
+/* returns books in given genre */
+app.get('/genre/:genre', (req, res) => {
+    let genre = req.params.genre;
+    genre = genre.toLowerCase();
+
+    //genre = genre.charAt(0).toUpperCase() + genre.slice(1); // 'romance' => 'Romance' // állhat több szóból egy műfaj!
+    const genreList = genre.split(' ');
+    genre = [];
+
+    for (let word of genreList) {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        genre.push(word)
+    }
+
+    genre = genre.join(' ');
+
+    const booksInGenre = [];
+
+    for (const book of books.results) {
+        if (eval(book.genres).includes(genre)) { // eval: "['Horror']" => ['Horror']
+            booksInGenre.push(book);
+        }
+    }
+
+    res.json(booksInGenre); // returns a list of objects
+})
+
+/* returns a random book in given genre */
+app.get('/genre/:genre/random', (req, res) => {
+    let genre = req.params.genre;
+    genre = genre.toLowerCase();
+
+    const genreList = genre.split(' ');
+    genre = [];
+
+    for (let word of genreList) {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        genre.push(word)
+    }
+
+    genre = genre.join(' ');
+
+    const booksInGenre = [];
+
+    for (const book of books.results) {
+        if (eval(book.genres).includes(genre)) { // eval: "['Horror']" => ['Horror']
+            booksInGenre.push(book);
+        }
+    }
+
+    res.json(booksInGenre[Math.floor(Math.random()*booksInGenre.length)]); //returns an object
+})
+
+/* returns X random books in given genre */
+app.get('/genre/:genre/:num', (req, res) => {
+    let genre = req.params.genre;
+    let num = req.params.num;
+    genre = genre.toLowerCase();
+
+    const genreList = genre.split(' ');
+    genre = [];
+
+    for (let word of genreList) {
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        genre.push(word)
+    }
+
+    genre = genre.join(' ');
+
+    const booksInGenre = [];
+
+    for (const book of books.results) {
+        if (eval(book.genres).includes(genre)) { // eval: "['Horror']" => ['Horror']
+            booksInGenre.push(book);
+        }
+    }
+
+    res.json(booksInGenre.slice(0, num)); // returns a list of objects
+})
+
+/* returns the books of given author */
+app.get('/author/:name', (req, res) => {
+    let authorName = req.params.name;
+    // lehet ilyen is egy szerző neve: "Seth Harwood (Goodreads Author)"
+    // vagy több szerző esetén: "Anna Akhmatova, Paul Valet (Translator)"
+
+    const booksOfAuthor = [];
+
+    for (const book of books.results) {
+        if (book.author.toLowerCase().includes(authorName.toLowerCase())) {
+            booksOfAuthor.push(book);
+        }
+    }
+
+    res.json(booksOfAuthor); // returns a list of objects if author is found, else an empty list
+})
+
+/* returns the books which were published in given date */
+app.get('/publication-date/:date', (req, res) => {
+    let date = req.params.date;
+    // így néz ki: "October 1st 2015"
+
+    const booksPublished = [];
+
+    for (const book of books.results) {
+        if (book.publishDate.toString().slice(-4) == date) {
+            booksPublished.push(book);
+        }
+    }
+
+    res.json(booksPublished); // returns a list of objects
+})
+
+/* returns the books which have less than :num pages */
+app.get('/pages/:num', (req, res) => {
+    let num = req.params.num;
+
+    const searchedBooks = [];
+
+    for (const book of books.results) {
+        if (book.pages != "" && book.pages <= num) {
+            searchedBooks.push(book);
+        }
+    }
+
+    res.json(searchedBooks); // returns a list of objects
+})
+
+/* returns the books which cost less than :price */
+app.get('/price/:price', (req, res) => {
+    let price = req.params.price;
+    // ár így néz ki: 5.5, lehet üres string
+
+    const searchedBooks = [];
+
+    for (const book of books.results) {
+        if (book.price != "" && book.price <= price) {
+            searchedBooks.push(book);
+        }
+    }
+
+    res.json(searchedBooks); // returns a list of objects
+})
+
+/* returns books of given language */
+app.get('/language/:language', (req, res) => {
+    let language = req.params.language;
+
+    const booksOfLanguage = [];
+
+    for (const book of books.results) {
+        if (book.language.toLowerCase().includes(language.toLowerCase())) {
+            booksOfLanguage.push(book);
+        }
+    }
+
+    res.json(booksOfLanguage); // returns a list of objects
 })
 
 app.listen(process.env.PORT || 3000)
---
+++ index2.js
@@ -0,0 +1,48 @@
+import { books } from './books.js'
+import { books_with_images } from './books_with_images.js'
+import express from 'express'
+
+const app = express()
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err })
+})
+
+/* returns all the books without the images  */
+/*
+app.get('/', (req, res) => {
+    res.json(books.books)
+})
+*/
+
+/* returns a random book without the images  */
+app.get('/randombook', (req, res) => {
+    res.json(books.books[Math.floor(Math.random()*books.books.length)])
+})
+
+/* returns a random book */
+app.get('/random', (req, res) => {
+    res.json(books_with_images.results[Math.floor(Math.random()*books_with_images.results.length)]);
+})
+
+/* returns the first X books without the images  */
+app.get('/:num', (req, res) => {
+    const num = req.params.num;
+    res.json(books.books.slice(0, num))
+})
+
+/* returns random books of given number */
+app.get('/random/:num', (req, res) => {
+    const num = req.params.num;
+    const shuffled = books_with_images.results.sort(() => 0.5 - Math.random());
+    res.json(shuffled.slice(0, num));
+    console.log(res.status);
+})
+
+/* returns the first X books */
+app.get('/books/:num', (req, res) => {
+    const num = req.params.num;
+    res.json(books_with_images.results.slice(0,num));
+})
+
+app.listen(process.env.PORT || 3000)
